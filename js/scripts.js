d10a3522840d65dd6d9f0d1882c2206be4b6237c
Krista Rutz and Tiffany Siu <krista@>
2020-01-13 13:20:10 -0800

    add functions for add, sub, multiply, and divide

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 function add(number1,number2){
-
   return number1 + number2;
  }
- var number1 = parseInt(prompt("1st?"));
- var number2 = parseInt(prompt("2nd?"));
- alert("It is " + add(number1, number2));
+
+function subtract(number1, number2){
+  return number1 - number2;
+}
+
+function multiply(number1, number2){
+  return number1 * number2;
+}
+
+function divide(number1, number2){
+  return number1 / number2;
+}
+
+ var number1 = parseInt(prompt("1st number?"));
+ var number2 = parseInt(prompt("2nd number?"));
+ var result =  divide(number1, number2);
+ alert("It is " + result);
+
