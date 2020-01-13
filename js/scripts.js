c0883c565e4b5244c337687a327261ee50b0f251
Krista Rutz and Tiffany Siu <krista@>
2020-01-13 09:36:45 -0800

    initialize calculator project

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function add(number1,number2){
+
+  return number1 + number2;
+ }
+ var number1 = parseInt(prompt("1st?"));
+ var number2 = parseInt(prompt("2nd?"));
+ alert("It is " + add(number1, number2));
