d7d4553c6fee68b3a261f15446eb45f4dca28bd5
Tiffany Siu <tsiu88@gmail.com>
2020-01-15 15:26:45 -0800

    add jquery and bootstrap, change input and output from alerts to forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Business/back-end logic:
 function add(number1,number2){
   return number1 + number2;
  }
@@ -14,8 +15,15 @@ function divide(number1, number2){
   return number1 / number2;
 }
 
- var number1 = parseInt(prompt("1st number?"));
- var number2 = parseInt(prompt("2nd number?"));
- var result =  divide(number1, number2);
- alert("It is " + result);
-
+// User interface/front-end logic:
+$(document).ready(function(){
+  $("#add").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result =  add(number1, number2);
+  
+    $("#output").text(result);
+  });
+});
+ 
