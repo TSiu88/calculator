edebd7ae4de9ab946dc48dc38db3e8a9f9f92812
Erik Grove and Tiffany Siu <erik@>
2020-01-16 15:35:00 -0800

    edit code to have branching and delete repeating code

--- js/scripts.js
+++ js/scripts.js
@@ -17,38 +17,26 @@ function divide(number1, number2){
 
 // User interface/front-end logic:
 $(document).ready(function(){
-  $("#add").submit(function(event){
+  $("#calculator").submit(function(event){
     event.preventDefault();
     var number1 = parseInt($("#num1").val());
     var number2 = parseInt($("#num2").val());
-    var result =  add(number1, number2);
-  
-    $("#output").text(result);
-  });
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
 
-  $("#sub").submit(function(event){
-    event.preventDefault();
-    var number1 = parseInt($("#num3").val());
-    var number2 = parseInt($("#num4").val());
-    var result =  subtract(number1, number2);
-  
-    $("#output").text(result);
-  });
-
-  $("#multiply").submit(function(event){
-    event.preventDefault();
-    var number1 = parseInt($("#num5").val());
-    var number2 = parseInt($("#num6").val());
-    var result =  multiply(number1, number2);
-  
-    $("#output").text(result);
-  });
-
-  $("#divide").submit(function(event){
-    event.preventDefault();
-    var number1 = parseInt($("#num7").val());
-    var number2 = parseInt($("#num8").val());
-    var result =  divide(number1, number2);
+    if (operator === "add"){
+      result =  add(number1, number2);
+    } 
+    else if (operator === "subtract"){
+      result = subtract(number1, number2);
+    }
+    else if (operator === "multiply"){
+      result = multiply(number1, number2);
+    }
+    else if (operator === "divide"){
+      result = divide(number1, number2);
+    }
+    
   
     $("#output").text(result);
   });
